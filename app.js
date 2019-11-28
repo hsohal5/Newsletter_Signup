55b00acd6cdc8630507b2273b2ca9476725efd11
Harseerat Sohal <hsohal5@uwo.ca>
2019-11-28 18:07:56 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/", function(req,res){
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+  var data = {
+    members : [{
+      email_address : email,
+      status : "subscribed",
+      merge_fields : {
+        FNAME: firstName,
+        LNAME : lastName
+      }
+    }]
+  };
+ var jsonData = JSON.stringify(data);
+  var options = {
+    url : 'https://us4.api.mailchimp.com/3.0/lists/9aea59bfa1',
+    method : "POST",
+    headers: {
+      "Authorization": "seerat1 d23918718ae670916680aca73790ebc3-us4"
+    },
+    body : jsonData
+
+  };
+
+  request(options, function(error,response,body){
+    if(error){
+      res.sendFile(__dirname+"/failure.html");
+    }
+    else{
+      console.log(response.statusCode);
+      if(response.statusCode === 200){
+        res.sendFile(__dirname+"/success.html");
+      }else{
+        res.sendFile(__dirname+"/failure.html");
+      }
+    }
+  });
+});
+
+app.post("/failure",function(req,res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000,function(){
+  console.log("Server listening");
+});
+
+
+// d23918718ae670916680aca73790ebc3-us4
+
+// Audience // ID
+// 9aea59bfa1
